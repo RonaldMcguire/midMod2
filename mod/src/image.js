c8bd4deac96bf3048f59a4f6e23d0b443171b833
Ronald McGuire <ronaldmcguire@PSADMINs-MacBook-Air-86.local>
2020-07-26 17:25:32 -0400

    messed up and fix the div tags. added the card

--- mod/src/image.js
+++ mod/src/image.js
@@ -7,10 +7,14 @@ function Image() {
     <div className="Image">
         <div class="row row-cols-1 row-cols-md-3">
   <div class="col mb-4">
-    
+    <div class="card">
+      <img src="..." class="card-img-top" alt="..."></img>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+      </div>
     </div>
   </div>
-</div>
+  
 
     </div>
   );
