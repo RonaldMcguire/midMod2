004012ab7c66c75cd7df5723b61819b8acd4aab4
Ronald McGuire <ronaldmcguire@PSADMINs-MacBook-Air-86.local>
2020-07-27 19:41:34 -0400

    finishing the card title

--- mod/src/FirstProfile.js
+++ mod/src/FirstProfile.js
@@ -9,7 +9,9 @@ return (
             <div class="col mb-4">
                 <div class="card" >
                 <img src ={require('./photos/redmoth.jpg')} alt = ""  />
-
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                </div>
                 </div>
             </div>
         </div>
