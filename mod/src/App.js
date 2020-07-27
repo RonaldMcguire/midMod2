af36b11d27b67267fd45f626275cbf4526e1c8ad
Ronald McGuire <ronaldmcguire@PSADMINs-MacBook-Air-86.local>
2020-07-27 19:15:35 -0400

    adding the profile pictures

--- mod/src/App.js
+++ mod/src/App.js
@@ -10,27 +10,42 @@ import {
 export default function App() {
   return (
     <Router>
- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class = "fixNav">
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
       </button>
-      <Link class="navbar-brand" href="#">Navbar</Link>
+      
+      <Link class="navbar-brand" href="#">
+        daisy 
+      <img src ={require('')} alt = ""/>
+      </Link>
     
       <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
           <li class="nav-item active">
           </li>
           <li class="nav-item">
-            <Link class="nav-link" href="#">Link</Link>
+            <Link class="nav-link" href="#">
+              sandra
+            <img src ={require('')} alt = ""/>
+            </Link>
           </li>
           <li class="nav-item">
-            <Link class="nav-link disabled" href="#" tabindex="-1">Disabled</Link>
+            <Link class="nav-link" href="#" tabindex="-1">
+            araceli
+            <img src ={require('')} alt = ""/>
+
+            </Link>
           </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
         </form>
       </div>
-    </nav>
+      <img src ={require('./photos/logo.png')} alt = ""  />
+      </nav>
+    </div>
     
     <Image />
 
--- mod/src/image.js
+++ mod/src/image.js
@@ -6,30 +6,48 @@ function Image() {
   return (
     <div className="Image" id = "size">
         <div class="row row-cols-1" >
-  <div class="col mb-4">
-    <div class="card" >
-      <img src ={require('./photos/redmoth.jpg')} alt = ""  />
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-      </div>
-    </div>
-  </div>
-  <div class="col mb-4">
-  <div class="card" >
-      <img src ={require('./photos/redmoth.jpg')} alt = ""  />
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+            <div class="col mb-4">
+                <div class="card" >
+                <img src ={require('./photos/redmoth.jpg')} alt = ""  />
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                </div>
+                </div>
+            </div>
+            
+        <div class="col mb-4">
+            <div class="card" >
+                <img src ={require('./photos/theater.jpg')} alt = ""  />
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    </div>
+                </div>
+            </div>
+        
+
+        <div class="col mb-4">
+            <div class="card" >
+                <img src ={require('./photos/open.jpg')} alt = ""  />
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    </div>
+                </div>
+            </div>
+
+        <div class="col mb-4">
+            <div class="card" >
+                <img src ={require('./photos/clock.jpg')} alt = ""  />
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    </div>
+                </div>
+            </div>
         </div>
+        </div>
+    
 
-
-      </div>
-    </div>
-</div>
-
-
-    </div>
+        
   );
 }
 
 export default Image;
-
